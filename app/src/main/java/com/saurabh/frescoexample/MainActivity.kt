71bc966d62898e7f1a969db8d261814db1bc8529
Saurabh <saurabh.kumar021@gmail.com>
2020-09-08 19:22:21 +0530

    Initial commit

---
+++ app/src/main/java/com/saurabh/frescoexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.saurabh.frescoexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
